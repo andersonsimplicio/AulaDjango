573f296c178194dac58ec2468bac6cce6db316f0
andersonsimplicio <anderson25eu@gmail.com>
2024-03-05 20:12:23 -0300

    Arquivo .env

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t=t9%8(can1n565ng#+vj2-_amnb^9m!+tik%cfp&--v6ta2k9'
+SECRET_KEY = config('chave')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
